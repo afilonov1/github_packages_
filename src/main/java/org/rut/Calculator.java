8dea5cf139472ee8d38a98f3a4b863246f22e92f
filonov <filonov@lk-soft.ru>
2023-05-05 16:22:45 +0300

    first commit

---
+++ src/main/java/org/rut/Calculator.java
@@ -0,0 +1,16 @@
+package org.rut;
+
+public class Calculator {
+    public Double add(Double a, Double b) {
+        return a + b;
+    }
+    public Double sub(Double a, Double b) {
+        return a - b;
+    }
+    public Double div(Double a, Double b) {
+        return a / b;
+    }
+    public Double mul(Double a, Double b) {
+        return a * b;
+    }
+}
